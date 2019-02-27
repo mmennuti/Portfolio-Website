34c78f3c5ff75a3cd8f8e86aa63831cf11d2e466
Michael <mennuti@gmail.com>
2019-02-27 13:09:23 -0800

    Adding eslint configuration

--- portfolio.js
+++ portfolio.js
@@ -7,203 +7,208 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var Header = function (_React$Component) {
-	_inherits(Header, _React$Component);
+  _inherits(Header, _React$Component);
 
-	function Header(props) {
-		_classCallCheck(this, Header);
+  function Header(props) {
+    _classCallCheck(this, Header);
 
-		return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).call(this, props));
-	}
+    return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).call(this, props));
+  }
 
-	_createClass(Header, [{
-		key: "render",
-		value: function render() {
-			return React.createElement(
-				"div",
-				null,
-				React.createElement("img", { src: "./media/portrait.jpg", alt: "Self portrait of Michael Mennuti" }),
-				React.createElement(
-					"div",
-					null,
-					React.createElement(
-						"h1",
-						null,
-						"Michael Mennuti"
-					)
-				)
-			);
-		}
-	}]);
+  _createClass(Header, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement("img", {
+          src: "./media/portrait.jpg",
+          alt: "Self portrait of Michael Mennuti"
+        }),
+        React.createElement(
+          "div",
+          null,
+          React.createElement(
+            "h1",
+            null,
+            "Michael Mennuti"
+          )
+        )
+      );
+    }
+  }]);
 
-	return Header;
+  return Header;
 }(React.Component);
 
 var Tabs = function (_React$Component2) {
-	_inherits(Tabs, _React$Component2);
+  _inherits(Tabs, _React$Component2);
 
-	function Tabs(props) {
-		_classCallCheck(this, Tabs);
+  function Tabs(props) {
+    _classCallCheck(this, Tabs);
 
-		var _this2 = _possibleConstructorReturn(this, (Tabs.__proto__ || Object.getPrototypeOf(Tabs)).call(this, props));
+    var _this2 = _possibleConstructorReturn(this, (Tabs.__proto__ || Object.getPrototypeOf(Tabs)).call(this, props));
 
-		_this2.state = {
-			activeTab: props.activeTab
-		};
-		return _this2;
-	}
+    _this2.state = {
+      activeTab: props.activeTab
+    };
+    return _this2;
+  }
 
-	_createClass(Tabs, [{
-		key: "selectTab",
-		value: function selectTab(index, event) {
-			event.preventDefault();
-			this.setState({
-				activeTab: index
-			});
-		}
-	}, {
-		key: "_renderTitles",
-		value: function _renderTitles() {
-			function labels(child, index) {
-				var activeClass = this.state.activeTab === index ? 'active' : '';
-				return React.createElement(
-					"li",
-					{ key: index },
-					React.createElement(
-						"a",
-						{ href: "#",
-							className: activeClass,
-							onClick: this.selectTab.bind(this, index) },
-						child.props.label
-					)
-				);
-			}
+  _createClass(Tabs, [{
+    key: "selectTab",
+    value: function selectTab(index, event) {
+      event.preventDefault();
+      this.setState({
+        activeTab: index
+      });
+    }
+  }, {
+    key: "_renderTitles",
+    value: function _renderTitles() {
+      function labels(child, index) {
+        var activeClass = this.state.activeTab === index ? "active" : "";
+        return React.createElement(
+          "li",
+          { key: index },
+          React.createElement(
+            "a",
+            {
+              href: "#",
+              className: activeClass,
+              onClick: this.selectTab.bind(this, index)
+            },
+            child.props.label
+          )
+        );
+      }
 
-			return React.createElement(
-				"ul",
-				{ className: "tabs__labels" },
-				this.props.children.map(labels.bind(this))
-			);
-		}
-	}, {
-		key: "_renderContent",
-		value: function _renderContent() {
-			return React.createElement(
-				"div",
-				{ className: "tabs__content" },
-				this.props.children[this.state.activeTab]
-			);
-		}
-	}, {
-		key: "render",
-		value: function render() {
-			return React.createElement(
-				"div",
-				{ className: "tabs" },
-				this._renderTitles(),
-				this._renderContent()
-			);
-		}
-	}]);
+      return React.createElement(
+        "ul",
+        { className: "tabs__labels" },
+        this.props.children.map(labels.bind(this))
+      );
+    }
+  }, {
+    key: "_renderContent",
+    value: function _renderContent() {
+      return React.createElement(
+        "div",
+        { className: "tabs__content" },
+        this.props.children[this.state.activeTab]
+      );
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        { className: "tabs" },
+        this._renderTitles(),
+        this._renderContent()
+      );
+    }
+  }]);
 
-	return Tabs;
+  return Tabs;
 }(React.Component);
 
 var Pane = function (_React$Component3) {
-	_inherits(Pane, _React$Component3);
+  _inherits(Pane, _React$Component3);
 
-	function Pane(props) {
-		_classCallCheck(this, Pane);
+  function Pane(props) {
+    _classCallCheck(this, Pane);
 
-		return _possibleConstructorReturn(this, (Pane.__proto__ || Object.getPrototypeOf(Pane)).call(this, props));
-	}
+    return _possibleConstructorReturn(this, (Pane.__proto__ || Object.getPrototypeOf(Pane)).call(this, props));
+  }
 
-	_createClass(Pane, [{
-		key: "render",
-		value: function render() {
-			return React.createElement(
-				"div",
-				null,
-				this.props.children
-			);
-		}
-	}]);
+  _createClass(Pane, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        this.props.children
+      );
+    }
+  }]);
 
-	return Pane;
+  return Pane;
 }(React.Component);
 
 var textDevelopment = React.createElement(
-	"div",
-	null,
-	React.createElement(
-		"p",
-		null,
-		"I have been making software for about eleven years now. I studied computer science and game design at the Jack Baskin School of Engineering at the University of California, Santa Cruz."
-	),
-	React.createElement(
-		"p",
-		null,
-		"After I graduated with my B.S. in computer Science, I moved to Madison, Wisconsin to work at Epic, the leading electronic health record vendor. I worked there for seven years, designing and developing reporting and analytics software for medical organizations."
-	)
+  "div",
+  null,
+  React.createElement(
+    "p",
+    null,
+    "I have been making software for about eleven years now. I studied computer science and game design at the Jack Baskin School of Engineering at the University of California, Santa Cruz."
+  ),
+  React.createElement(
+    "p",
+    null,
+    "After I graduated with my B.S. in computer Science, I moved to Madison, Wisconsin to work at Epic, the leading electronic health record vendor. I worked there for seven years, designing and developing reporting and analytics software for medical organizations."
+  )
 );
 var textDesign = React.createElement(
-	"div",
-	null,
-	React.createElement(
-		"p",
-		null,
-		"During my studies in game design, and as a professional software developer, I've had the opportunity to develop my skills in user experience and graphic design."
-	)
+  "div",
+  null,
+  React.createElement(
+    "p",
+    null,
+    "During my studies in game design, and as a professional software developer, I've had the opportunity to develop my skills in user experience and graphic design."
+  )
 );
 var textIllustration = React.createElement(
-	"div",
-	null,
-	React.createElement(
-		"p",
-		null,
-		"I like to draw"
-	)
+  "div",
+  null,
+  React.createElement(
+    "p",
+    null,
+    "I like to draw"
+  )
 );
 
 var App = function (_React$Component4) {
-	_inherits(App, _React$Component4);
+  _inherits(App, _React$Component4);
 
-	function App() {
-		_classCallCheck(this, App);
+  function App() {
+    _classCallCheck(this, App);
 
-		return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+  }
 
-	_createClass(App, [{
-		key: "render",
-		value: function render() {
-			return React.createElement(
-				"div",
-				null,
-				React.createElement(Header, null),
-				React.createElement(
-					Tabs,
-					{ activeTab: 0 },
-					React.createElement(
-						Pane,
-						{ label: "Development" },
-						textDevelopment
-					),
-					React.createElement(
-						Pane,
-						{ label: "Design" },
-						textDesign
-					),
-					React.createElement(
-						Pane,
-						{ label: "Illustration" },
-						textIllustration
-					)
-				)
-			);
-		}
-	}]);
+  _createClass(App, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(Header, null),
+        React.createElement(
+          Tabs,
+          { activeTab: 0 },
+          React.createElement(
+            Pane,
+            { label: "Development" },
+            textDevelopment
+          ),
+          React.createElement(
+            Pane,
+            { label: "Design" },
+            textDesign
+          ),
+          React.createElement(
+            Pane,
+            { label: "Illustration" },
+            textIllustration
+          )
+        )
+      );
+    }
+  }]);
 
-	return App;
+  return App;
 }(React.Component);
 
 ReactDOM.render(React.createElement(App, null), document.querySelector("#app"));
