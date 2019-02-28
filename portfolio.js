ff9175fd194cd7cd5f36d863f1a14dc12cf71041
Michael <mennuti@gmail.com>
2019-02-27 20:34:17 -0800

    Filling out the content a bit

--- portfolio.js
+++ portfolio.js
@@ -145,9 +145,20 @@ var Pane = function (_React$Component3) {
   return Pane;
 }(React.Component);
 
+// Put this in an array to make it easier to edit later. Could eventually pull from some sort of database, but that seems like overkill unless I start adding details to each.
+
+
+var technologies = ["C#", "VisualBasic 6", "M(UMPS)", "Microsoft SQL Server", "ASP.NET", "WebForms", "JavaScript", "TypeScript", "jQuery", "AJAX React.js", "HTML5", "CSS3", "SCSS", "Unity3D", "C++", "OpenGL", "Java", "Python", "C", "SVN", "git"];
+
+var techList = technologies.map(function (tech, index) {
+  return React.createElement(
+    "li",
+    { key: index },
+    tech
+  );
+});
+
 // This is the markup for the text of the development pane.
-
-
 var textDevelopment = React.createElement(
   "div",
   null,
@@ -159,7 +170,21 @@ var textDevelopment = React.createElement(
   React.createElement(
     "p",
     null,
-    "After I graduated with my B.S. in computer Science, I moved to Madison, Wisconsin to work at Epic, the leading electronic health record vendor. I worked there for seven years, designing and developing reporting and analytics software for medical organizations."
+    "After I graduated with my B.S. in computer Science, I moved to Madison, Wisconsin to work at Epic, the leading electronic health record vendor. I worked there for seven years, designing and developing reporting and analytics software for medical organizations. I particularly specialized in integration projects, linking functionality between our various reporting tools, but I've also worked on backend database work, web services, front end implementation, and various sorts of optimization.",
+    React.createElement(
+      "p",
+      null,
+      "Some of the technologies I've used include:"
+    ),
+    React.createElement(
+      "p",
+      null,
+      React.createElement(
+        "ul",
+        null,
+        techList
+      )
+    )
   )
 );
 
@@ -170,7 +195,12 @@ var textDesign = React.createElement(
   React.createElement(
     "p",
     null,
-    "During my studies in game design, and as a professional software developer, I've had the opportunity to develop my skills in user experience and graphic design."
+    "During my studies in game design, and as a professional software developer, I've had the opportunity to develop skills in user experience and graphic design. After all, what is the point of a game if not to ellicit a particular experience in a player? I've worked on all stages of design: requirements gathering, wireframing, prototyping, usability heuristic analysis, user testing, iteration, and of course implementation."
+  ),
+  React.createElement(
+    "p",
+    null,
+    "I've even worked in design for accessibility, both for folks with dexterity limitations who rely on keyboard controls to navigate and application, and for people with visual disabilities. That includes colorblind users (like myself), those who require high contrast interfaces, and even fully blind users. Designing an application to be usable through a screen reader really changes how you think about usability."
   )
 );
 
@@ -181,7 +211,7 @@ var textIllustration = React.createElement(
   React.createElement(
     "p",
     null,
-    "I like to draw"
+    "For as long as I can remember I've loved to draw, paint, sculpt, really create any visual media. When I learned I was colorblind, I was discouraged from pursuing a career in art, but I persisted in doodling in the margins of my notes. In the last few years, I've started taking my drawing more seriously. Now in my spare time, I'm studying figure drawing, artistic anatomy, portraiture, draftsmanship, vehicle design, and pretty much anything else that tickles my fancy."
   )
 );
 
--- src/portfolio.jsx
+++ src/portfolio.jsx
@@ -89,8 +89,35 @@ class Pane extends React.Component {
   }
 }
 
+// Put this in an array to make it easier to edit later. Could eventually pull from some sort of database, but that seems like overkill unless I start adding details to each.
+const technologies = [
+  "C#",
+  "VisualBasic 6",
+  "M(UMPS)",
+  "Microsoft SQL Server",
+  "ASP.NET",
+  "WebForms",
+  "JavaScript",
+  "TypeScript",
+  "jQuery",
+  "AJAX React.js",
+  "HTML5",
+  "CSS3",
+  "SCSS",
+  "Unity3D",
+  "C++",
+  "OpenGL",
+  "Java",
+  "Python",
+  "C",
+  "SVN",
+  "git"
+]
+
+const techList = technologies.map((tech, index) => <li key={index}>{tech}</li>)
+
 // This is the markup for the text of the development pane.
-var textDevelopment = (
+const textDevelopment = (
   <div>
     <p>
       I have been making software for about eleven years now. I studied computer
@@ -101,26 +128,53 @@ var textDevelopment = (
       After I graduated with my B.S. in computer Science, I moved to Madison,
       Wisconsin to work at Epic, the leading electronic health record vendor. I
       worked there for seven years, designing and developing reporting and
-      analytics software for medical organizations.
+      analytics software for medical organizations. I particularly specialized
+      in integration projects, linking functionality between our various
+      reporting tools, but I've also worked on backend database work, web
+      services, front end implementation, and various sorts of optimization.
+      <p>Some of the technologies I've used include:</p>
+      <p>
+        <ul>{techList}</ul>
+      </p>
     </p>
   </div>
 )
 
 // This is the markup for the text of the design pane.
-var textDesign = (
+const textDesign = (
   <div>
     <p>
       During my studies in game design, and as a professional software
-      developer, I've had the opportunity to develop my skills in user
-      experience and graphic design.
+      developer, I've had the opportunity to develop skills in user experience
+      and graphic design. After all, what is the point of a game if not to
+      ellicit a particular experience in a player? I've worked on all stages of
+      design: requirements gathering, wireframing, prototyping, usability
+      heuristic analysis, user testing, iteration, and of course implementation.
+    </p>
+    <p>
+      I've even worked in design for accessibility, both for folks with
+      dexterity limitations who rely on keyboard controls to navigate and
+      application, and for people with visual disabilities. That includes
+      colorblind users (like myself), those who require high contrast
+      interfaces, and even fully blind users. Designing an application to be
+      usable through a screen reader really changes how you think about
+      usability.
     </p>
   </div>
 )
 
 // This is the markup for the text of the illustration pane.
-var textIllustration = (
+const textIllustration = (
   <div>
-    <p>I like to draw</p>
+    <p>
+      For as long as I can remember I've loved to draw, paint, sculpt, really
+      create any visual media. When I learned I was colorblind, I was
+      discouraged from pursuing a career in art, but I persisted in doodling in
+      the margins of my notes. In the last few years, I've started taking my
+      drawing more seriously. Now in my spare time, I'm studying figure drawing,
+      artistic anatomy, portraiture, draftsmanship, vehicle design, and pretty
+      much anything else that tickles my fancy.
+    </p>
   </div>
 )
 
